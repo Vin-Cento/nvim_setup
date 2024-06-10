faef32dd062f25683c447fef3b4a94d121221a0b
Vincent <vincentliu362@gmail.com>
2024-06-10 00:05:48 -0500

    rebind: use ctrl-\ to toggle terminal

--- lua/FTerm-config/init.lua
+++ lua/FTerm-config/init.lua
@@ -7,7 +7,7 @@ require 'FTerm'.setup({
 })
 
 -- Example keybindings
-vim.keymap.set('n', '<A-i>', '<cmd>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('n', '<C-\\>', '<cmd>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<C-\\>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
 vim.keymap.set('n', 'gl', '<cmd>lua require("FTerm").scratch({cmd = "lazygit"})<cr>')
